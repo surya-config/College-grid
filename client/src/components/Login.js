bbb0e391680fb43b4d275e843252a0c0879cd3d6
Surya <a1b2c3d4config@gmail.com>
2020-11-11 18:13:30 +0530

    login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,12 +2,17 @@ import React, { useState } from "react";
 import Header from "./Header";
 import { Input, Button } from "react-bootstrap";
 
+import PersonIcon from "@material-ui/icons/Person";
+
 import "../css/Login.css";
 
 function Login() {
   const [username, setUsername] = useState("");
+  const [usn, setUsn] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
+
+  const [switchBar, setSwitchBar] = useState(false);
 
   //   const signIn = (event) => {
   //     event.preventDefault();
@@ -17,8 +22,13 @@ function Login() {
   //     setOpen(false);
   //   };
   return (
-    <div>
-      <form className="app__signup">
+    <div className="login">
+      {/* <form className="app__signup">
+        <img
+          className="app__logo"
+          src="https://pngimage.net/wp-content/uploads/2019/05/video-call-logo-png-2.png"
+          alt="Face Time"
+        />
         <h2>Login</h2>
         <input
           placeholder="Username"
@@ -39,7 +49,77 @@ function Login() {
         />
 
         <Button type="submit">Sign In</Button>
-      </form>
+      </form> */}
+      {switchBar === false ? (
+        <div className="login__container">
+          <div className="login__leftContainer">
+            <div className="login__imageOverlay"></div>
+            <div className="login__leftContainerContent">
+              <h1>Welcome Back!</h1>
+              <button onClick={() => setSwitchBar(true)}>Sign in</button>
+            </div>
+          </div>
+          <div className="login__rightContainer">
+            <div className="login__rightContainerContent">
+              <h2>Create Account</h2>
+              <input
+                placeholder="Name"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <input
+                placeholder="USN"
+                value={usn}
+                onChange={(e) => setUsn(e.target.value)}
+              />
+              <input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+
+              <Button type="submit">Sign up</Button>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="signup__container">
+          <div className="signup__leftContainer">
+            <div className="signup__leftContainerContent">
+              <h2>Login</h2>
+              <input
+                placeholder="Usn"
+                value={usn}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+
+              <Button type="submit">Sign In</Button>
+            </div>
+          </div>
+
+          <div className="signup__rightContainer">
+            <div className="signup__imageOverlay"></div>
+            <div className="signup__rightContainerContent">
+              <h1>Hello Friend!</h1>
+              <button onClick={() => setSwitchBar(false)}>Sign up</button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
