48a23ef25251fa1b32893e6e1706d22d50a54032
surya-config <a1b2c3d4config@gmail.com>
2022-05-05 00:15:12 +0530

    added a fix for redux

--- client/src/store.js
+++ client/src/store.js
@@ -7,8 +7,11 @@ const store = createStore(
   rootReducer,
   initialState,
   compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(thunk),
+    typeof window.__REDUX_DEVTOOLS_EXTENSION__ === "undefined"
+      ? a => a
+      : window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 export default store;
